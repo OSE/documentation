b624ec2cfc18347141d32b141125b0971a6518e7
Daniel Benamy <daniel@benamy.info>
2013-10-27 01:36:36 -0400

    Get json instead of xml & simplify.

--- bom_generator.py
+++ bom_generator.py
@@ -1,68 +1,61 @@
+import json
 import logging
 import re
-import requests
 import sys
-import xml.etree.ElementTree as ET
-from xml.sax.saxutils import unescape
+import urllib2
 
 
 def main(argv):
     if '-d' in argv:
         logging.basicConfig(level=logging.DEBUG)
     parts = {} # see add_part() for structure
-    URLS = [ # dozuki guides as xml
-        'https://opensourceecology.dozuki.com/api/2.0/guides/85?type=xml', # bottom frame
-        'https://opensourceecology.dozuki.com/api/2.0/guides/86?type=xml', # top frame
-        'https://opensourceecology.dozuki.com/api/2.0/guides/84?type=xml', # overall machine
+    DOZUKI_GUIDE_IDS = [
+        85, # bottom frame
+        86, # top frame
+        84, # overall machine
     ]
-    for url in URLS:
+    BASE_URL = 'https://opensourceecology.dozuki.com/api/2.0/guides'
+    for guide_id in DOZUKI_GUIDE_IDS:
+        url = "%s/%s" % (BASE_URL, guide_id)
         process_url(parts, url)
     output_bom(parts)
 
 
 def process_url(parts, url):
-    xml_str = requests.get(url).text
+    json_str = urllib2.urlopen(url).read()
+    guide = json.loads(json_str)
 
-    logging.debug(xml_str)
+    logging.debug(json.dumps(guide, sort_keys=True, indent=4, separators=(',', ': ')))
     logging.debug('----------------')
 
-    # Strip namespace to make the xml tree easier to work with
-    xml_str = re.sub(' xmlns="[^"]+"', '', xml_str, count=1)
-
-    extract_parts(parts, url, xml_str)
+    extract_parts(parts, guide)
 
 
-def extract_parts(parts, url, xml_str):
-    root = ET.fromstring(xml_str)
-
+def extract_parts(parts, guide):
     parts_level = None
-    lines = root.findall('./steps/step/lines/line')
-    for line in lines:
-        line_level = int(line.get('level'))
-        if parts_level is not None:
-            if line_level == parts_level:
-                process_part_line(parts, url, line.text)
-            else:
-                parts_level = None
-        elif line.text.startswith('Parts'):
-            parts_level = line_level + 1
+    for step in guide['steps']:
+        for line in step['lines']:
+            if parts_level is not None:
+                if line['level'] == parts_level:
+                    process_part_line(parts, guide, step, line['text_raw'])
+                else:
+                    parts_level = None
+            elif line['text_raw'].startswith('Parts'):
+                parts_level = line['level'] + 1
 
 
 # Part lines must start with a number, then have a space, then have the part
 # name. If you want to include more info, put it in parenthases after the
 # part name.
-def process_part_line(parts, url, line_text):
-    clean = unescape(line_text, {'&quot;': '"'})
-    logging.debug("Cleaned line: %s" % clean)
-    match = re.match('([0-9]+) ([^(]+)', clean, re.MULTILINE) # match checks only beginning of line
+def process_part_line(parts, guide, step, line_text):
+    match = re.match('([0-9]+) ([^(]+)', line_text, re.MULTILINE) # match checks only beginning of line
     if not match:
-        logging.error("Can't understand part line from %s: '%s'" % (url, line_text))
-        logging.error("Cleaned it looks like: '%s'" % clean)
+        logging.error("Can't understand part line from '%s', step %s: '%s'" % (guide['title'], step['orderby'], line_text))
         return
     count = int(match.group(1).strip())
     name = match.group(2).strip()
     logging.debug("%s %s" % (count, name))
-    add_part(parts, url, name, count)
+    add_part(parts, guide['title'], name, count)
 
 
 def add_part(parts, used_by, name, count):
