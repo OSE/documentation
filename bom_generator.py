826e1f956c133f325a5e3d46107ed5619f96f0f2
Daniel Benamy <daniel@benamy.info>
2013-10-27 02:18:37 -0400

    Support float quantities, dedupe BOM.

--- bom_generator.py
+++ bom_generator.py
@@ -48,17 +48,20 @@ def extract_parts(parts, guide):
 # name. If you want to include more info, put it in parenthases after the
 # part name.
 def process_part_line(parts, guide, step, line_text):
-    match = re.match('([0-9]+) ([^(]+)', line_text, re.MULTILINE) # match checks only beginning of line
+    match = re.match('([0-9.]+) ([^(]+)', line_text, re.MULTILINE) # match checks only beginning of line
     if not match:
         logging.error("Can't understand part line from '%s', step %s: '%s'" % (guide['title'], step['orderby'], line_text))
         return
-    count = int(match.group(1).strip())
+    count = float(match.group(1).strip())
     name = match.group(2).strip()
     logging.debug("%s %s" % (count, name))
     add_part(parts, guide['title'], name, count)
 
 
 def add_part(parts, used_by, name, count):
+    if name[-1] != 's':
+        name = name + 's'
+
     if name not in parts:
         parts[name] = {'count': 0, 'used_by': set()}
     parts[name]['count'] += count
@@ -66,11 +69,28 @@ def add_part(parts, used_by, name, count):
 
 
 
+def plural(num, text):
+    return "%d %s%s" % (num, text, "s"[num==1:])
+
+
 def output_bom(parts):
-    for part in parts:
-        print("Count: %s, Part: %s" % (parts[part]['count'], part))
+    # for part in sorted(parts.keys(), cmp=cmp_by_last_word):
+    for part in sorted(parts.keys(), key=lambda x: x.split()[-1] + x):
+        count = parts[part]['count']
+        if float(round(count)) == count:
+            count = int(count)
+        print("Count: %s, Part: %s" % (count, part))
         for used_by in parts[part]['used_by']:
             print("    Used by: %s" % used_by)
+
+
+def cmp_by_first_word(a, b):
+    regex = '([^a-zA-Z]*)(.*)'
+    a_chunks = list(re.match(regex, a).groups())
+    a_chunks.reverse()
+    b_chunks = list(re.match(regex, b).groups())
+    b_chunks.reverse()
+    return ''.join(a_chunks) < ''.join(b_chunks)
 
 
 main(sys.argv)
